e2d6d31387a36f885c62209710e639bd73351023
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-11-15 20:13:18 +0900

    Update SERVER.java

--- SERVER/SERVER.java
+++ SERVER/SERVER.java
@@ -1,26 +1,115 @@
 package SERVER;
 
-import java.util.Scanner;
-//DB를 이용하려면 반드시 사용 해당 클래스를 통해서 프론트와 연결.
-public class SERVER {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ConnectServer cs = new ConnectServer();
-        String cmd = sc.nextLine();
+import java.sql.*;
 
-        //sign up 버튼을 누를 시에만 실행되게 하기.
-        if(cmd.equals("sign up")) { //중복되면 에러 남기고 안집어넣음.
-            cs.InputData(2019038054, 2019038054); //id pw 변수화
+public class ConnectServer {
+    private Connection conn;
+    public ConnectServer() { // 서버 연결 함수
+        String url = "jdbc:mysql://localhost:3306/marketeam"; //서버
+        String user = "root";           //유저명
+        String password = "7749";       //비번
+
+        try { //실행구간
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.conn = DriverManager.getConnection(url, user, password);
+            System.out.println("연결 성공!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+            e.printStackTrace();
         }
-        //login 버튼 시 DB에서 id 찾고 해당 id의 pw비교하여 일치 하면 true
-        //id, pw 일치 하지 않을 시 false.
-        else if (cmd.equals("login")) {
-            boolean flag = cs.FindID(2019038054, 2019038054); //id pw 변수화
-            if(flag) System.out.println("아이디 존재.");
+    }//public ConnectServer
+
+    public void DisconnectServer() { //서버 연결 해제
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("연결 끊기");
+            } catch (SQLException e) {
+            }
         }
+    }//disconnectserver
+
+    public void InputData(int id, int pw) { //signup 데이터 삽입 함수
+        try {
+            //매개변수화된 SQL문 작성
+            String sql = "INSERT INTO login (id, pw) VALUES (?,?)";
+            //매개변수화된 SQL문 실행 메소드 pstmt
+            PreparedStatement pstmt = this.conn.prepareStatement(sql);
+            //변수 삽입
+            pstmt.setInt(1, id);
+            pstmt.setInt(2, pw);
+            //데이터 저장 확인
+            int rows = pstmt.executeUpdate();
+            System.out.println("저장된 행 : " + rows);
+            pstmt.close();
+        } catch (SQLException e) {
+            System.out.println("SQLException");
+            e.printStackTrace();
+            //이미 있는 아이디입니다. 출력.
+        }
+    }//inputdata
+
+    public boolean FindID(int id, int pw){ //login 데이터를 가져오는 함수
+        try {
+            String sql = "SELECT id, pw FROM login WHERE id=?";
+            PreparedStatement pstmt = this.conn.prepareStatement(sql);
+            pstmt.setInt(1, id); //입력한 id를 통해서 서버와 확인
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()){ //일치하는 id를 찾아도 pw가 일치하지 않으면 false 되야함.
+                User user = new User();
+                user.setID(rs.getInt("id"));
+                user.setPW(rs.getInt("pw"));
+                System.out.println(user.toString());
+                pstmt.close();
+                return true;
+            } else {
+                System.out.println("존재하지 하지 않음");
+                pstmt.close();
+                return false;
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }//FindID
+
+    public void CheckList(int id){
+        try {
+            //매개변수화된 SQL문 작성
+            String sql = "INSERT INTO user_info () VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement pstmt = this.conn.prepareStatement(sql);
+            //pstmt.setInt(4, id); //입력한 id를 통해서 서버와 확인
+            //변수 삽입
+            pstmt.setString(1, "소프트웨어학과"); //major
+            //int i=0;
+            pstmt.setInt(2, 2); //grade (0:professor)
+            pstmt.setInt(3, 0); //gender (0:male 1:female)
+            pstmt.setInt(4, id); //id (number)
+            pstmt.setInt(5, 1); //leader
+            pstmt.setInt(6, 1); //announce
+            pstmt.setInt(7, 3); //ppt
+            pstmt.setInt(8, 3); //frontend
+            pstmt.setInt(9, 4); //backend
+            pstmt.setInt(10, 3); //selfability
+            // setInt 부분 반복문으로 줄일 것.
+            pstmt.setBoolean(11, false); //teammate (0:false, 1:true)
+
+            int rows = pstmt.executeUpdate();
+            System.out.println("저장된 행 : " + rows);
+            pstmt.close();
+
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    } //CheckList
 
 
-
-        cs.DisconnectServer();
-    }
 }
