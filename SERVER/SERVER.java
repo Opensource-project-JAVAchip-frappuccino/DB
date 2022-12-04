c679537da1cf3276c40067dbc83c3b208ca1f189
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-12-04 19:24:11 +0900

    created get function

--- SERVER/SERVER.java
+++ SERVER/SERVER.java
@@ -1,4 +1,4 @@
-package SERVER;
+//package SERVER;
 /*
  * 함수 건들지 말 것
  * 함수 건들지 말 것
@@ -12,16 +12,15 @@ package SERVER;
 import java.util.Scanner;
 //DB를 이용하려면 반드시 사용 해당 클래스를 통해서 프론트와 연결.
 public class SERVER {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ConnectServer cs = new ConnectServer();
         String cmd = sc.nextLine();
 
         int id = 2019038054;
-        String major = "software";
+        String major = "소프트웨어학과";
         int[] user_info = {id, 1, 2, 3, 4, 5, 6, 7, 0};
-        /*
+
         //sign up 버튼을 누를 시에만 실행되게 하기.
         if(cmd.equals("sign up")) { //중복되면 에러 남기고 안집어넣음.(자동처리)
             cs.InputData(id, id);               //id pw 변수화
@@ -35,15 +34,23 @@ public class SERVER {
         }
 
         else if( cmd.equals("check")){   //학과는 String 나머지는 int
-            cs.CheckList(user_info, major);
+            cs.CheckList(user_info, major, "김경민");
         }
 
         else if(cmd.equals("rewrite")){
-            cs.RewriteInfo(id);
+            cs.RewriteInfo(id, 1);
+        }
+        else if(cmd.equals("get")){
+            int var = cs.GetUserinfo(id, 10);
+            System.out.println(var);
+            String str = cs.GetUserinfo(id, "major");
+            System.out.println(str);
+        }
+        else if(cmd.equals("course")) {
+            cs.SetCourse(2019038053);
         }
 
-        cs.DisconnectServer();
 
-        */
+        cs.DisconnectServer();
     }
 }
