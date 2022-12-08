3f88f92ac86d9320384b9b0d5868875aa241727a
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-12-08 15:33:10 +0900

    미사용

--- SERVER/User.java
+++ SERVER/User.java
@@ -1,10 +1,11 @@
+/*
 package SERVER;
 //사용자에 대한 정보
 public class User {
     private int id;
     private int pw;
     private String major;
-    /* 1:id, 2:grade, 3:leader 4:announce 5:ppt 6:frontend 7:backend 8:selfability 9:teammate*/
+    // 1:id, 2:grade, 3:leader 4:announce 5:ppt 6:frontend 7:backend 8:selfability 9:teammate
     private int user_info[] = new int[9];
 
     public void setID(int id){
@@ -17,3 +18,4 @@ public class User {
         return "User("+ id + ","+ pw + ")";
     }
 }
+*/
