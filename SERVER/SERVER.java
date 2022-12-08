00e75125d381f1125f9b938d18bc0e7a039e6378
WhoLook <51149853+NyanPunch@users.noreply.github.com>
2022-12-08 15:37:05 +0900

    Update SERVER.java

--- SERVER/SERVER.java
+++ SERVER/SERVER.java
@@ -21,6 +21,8 @@ public class SERVER {
         String major = "소프트웨어학과";
         int[] user_info = {id, 1, 2, 3, 4, 5, 6, 7, 0};
 
+        String[] src = {"과목", "재구", "자바", "선과목"};
+
         //sign up 버튼을 누를 시에만 실행되게 하기.
         if(cmd.equals("sign up")) { //중복되면 에러 남기고 안집어넣음.(자동처리)
             cs.InputData(id, id);               //id pw 변수화
@@ -38,7 +40,7 @@ public class SERVER {
         }
 
         else if(cmd.equals("rewrite")){
-            cs.RewriteInfo(id, 1);
+            //cs.RewriteInfo(id, 1, arr);
         }
         else if(cmd.equals("get")){
             int var = cs.GetUserinfo(id, 10);
@@ -47,9 +49,20 @@ public class SERVER {
             System.out.println(str);
         }
         else if(cmd.equals("course")) {
-            cs.SetCourse(2019038053);
+            cs.SetCourse(src,0,0);
         }
+        else if(cmd.equals("title")){
+            cs.SetTitle(id, "제목을 입력하세요", "게시글은 이렇게 쓸 수 있고 최대 200자 까지 쓸 수 있습니다. 근데 얼마나 적히는지 제대로 알 순 없어요.");
+            String str = cs.GetTitle(id, "title");
+            System.out.println(str);
+            str = cs.GetTitle(id, "content");
+            System.out.println(str);
+        }
+        //String str = cs.GetCourse(1, "name");
+        //System.out.println(str);
 
+        //int r = cs.subject_row_size();
+        //System.out.println(r);
 
         cs.DisconnectServer();
     }
